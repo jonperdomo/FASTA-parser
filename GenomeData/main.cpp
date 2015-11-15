cecb17b3682cfa06ffaf38bbd54dbccafff44bd9
Jonathan Perdomo <perdomojonathan5@gmail.com>
2015-11-15 18:41:43 -0500

    Initial Commit

---
+++ GenomeData/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GenomeData
+//
+//  Created by jperdomo on 11/15/15.
+//  Copyright © 2015 jperdomo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
